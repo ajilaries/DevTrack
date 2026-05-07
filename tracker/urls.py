f9a44e831a98c5aa7711b789fa5a2ffbf534ddc1
Aju <ajilaries20@gmail.com>
2026-05-07 09:41:05 +0530

    urls gives the paths and routing

---
+++ tracker/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('',views.landing , name='landing'),
+    path('signup/',views.signup_view, name='signpu'),
+    path('login/',views.login_view,name='login'),
+    path('logout/',views.logout_view, name='logout'),
+    path('home/', views.home, name='home'),
+    path('add/',views.add_log, name='add_log'),
+    path('delete/<int:id>/', views.delete_log, name='delete_log'),
+    path('edit/<int:id>/',views.edit_log, name='edit_log')
+]
