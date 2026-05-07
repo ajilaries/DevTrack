e274081d15f7d54179a0a7b22e90cefbb1a85940
Aju <ajilaries20@gmail.com>
2026-05-07 10:26:29 +0530

    routing for the web application

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.landing , name='landing'),
     path('signup/',views.signup_view, name='signpu'),
     path('login/',views.login_view,name='login'),
+    path('/dashboard/',views.dashboard,name='dashboard'),
     path('logout/',views.logout_view, name='logout'),
     path('home/', views.home, name='home'),
     path('add/',views.add_log, name='add_log'),
