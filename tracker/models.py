ec9e3011c7da2124590341cedc5c26ab924476e8
Aju <ajilaries20@gmail.com>
2026-05-08 11:53:14 +0530

    OnetoOne profile model has implemented

--- tracker/models.py
+++ tracker/models.py
@@ -12,3 +12,37 @@ class StudyLog(models.Model):
     hours=models.IntegerField()
     notes=models.TextField()
     date=models.DateField(auto_now_add=True)
+
+# OneToOneField means one user one profile which is good for user profiles, settings and prefernces
+class Profile(models.Model):
+    user=models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+
+    )
+
+    bio=models.TextField(
+        blank=True
+    )
+    
+    github=models.URLField(
+        blank=True
+    )
+    linkedin=models.URLField(
+        blank=True
+    )
+
+    ROLE_CHOICES=(
+        ('student','Student'),
+        ('admin','Admin'),
+        ('mentor','Mentor')
+    )
+
+    role=models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+
+    def __str__(self):
+        return self.user.username
