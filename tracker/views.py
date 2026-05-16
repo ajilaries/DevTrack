ec36e2f5207efb6df81c61f0341fc49236a93ffc
Aju <ajilaries20@gmail.com>
2026-05-16 09:06:35 +0530

    views contains the viewset implementation

--- tracker/views.py
+++ tracker/views.py
@@ -15,6 +15,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .serializers import StudyLogSerializer
 from rest_framework import generics
+from rest_framework import viewsets
 
 class StudyLogAPIView(
     generics.ListCreateAPIView
@@ -300,5 +301,25 @@ class SingleStudyLogAPIView(
 
     def get_queryset(self):
         return StudyLog.objects.filter(
+            user=self.request.user
+        )
+    
+
+class StudyLogViewSet(
+    viewsets.ModelViewSet
+):
+    
+    serializer_class=StudyLogSerializer
+
+    permission_classes =[IsAuthenticated]
+
+    lookup_field='id'
+
+    def get_queryset(self):
+        return StudyLog.objects.filter(
+            user=self.request.user
+        )
+    def perform_create(self,serializer):
+        serializer.save(
             user=self.request.user
         )
