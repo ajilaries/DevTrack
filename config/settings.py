ae071d3560b969bcaa8262eb2fcb6f78835e87ac
Aju <ajilaries20@gmail.com>
2026-05-16 17:31:06 +0530

    pagination and the filters named filter search filter and the ordering filter has implemented

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tracker',
     'rest_framework',
+    'django-filters'
 ]
 
 MIDDLEWARE = [
@@ -141,5 +142,16 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
 
     ),
+    'DEFAULT_FILTER_BACKENDS':[
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE':5,
 
 }
