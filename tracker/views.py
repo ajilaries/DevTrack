fdd72a505ca686c8743482ba988f25c840ec5c1c
Aju <ajilaries20@gmail.com>
2026-05-20 19:19:35 +0530

    updated the get_queryset inside the notification viewset for proper notifications

--- tracker/views.py
+++ tracker/views.py
@@ -427,11 +427,13 @@ class NotificationViewSet(
     ]
 
     def get_queryset(self):
+        if getattr(self,'swagger_fake_view',False):
+            return Notification.objects.none()
+        
         return Notification.objects.filter(
             user=self.request.user
 
         ).order_by('-created_at')
-    
     @action(
         detail=True,
         methods=['POST']
